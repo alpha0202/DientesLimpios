8dc5eebc57e1ff7937411f7cea003d11429d800c
edwin harley martinez gomez <alpha0202@gmail.com>
2025-09-27 18:37:01 -0500

    agregando cqrs

---
+++ DientesLimpios.Aplicacion/CasosDeUso/Consultorio/Comandos/CrearConsultorio/CasoDeUsoCrearConsultorio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Aplicacion.CasosDeUso.Consultorio.Comandos.CrearConsultorio
+{
+    public class CasoDeUsoCrearConsultorio
+    {
+        public async Task<Guid> Handle(ComandoCrearConsultorio comando)
+        {
+            //orquestación de las acciones a realizar
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ DientesLimpios.Aplicacion/CasosDeUso/Consultorio/Comandos/CrearConsultorio/ComandoCrearConsultorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Aplicacion.CasosDeUso.Consultorio.Comandos.CrearConsultorio
+{
+    public class ComandoCrearConsultorio
+    {
+
+        public required string Nombre { get; set; }
+
+    }
+}
